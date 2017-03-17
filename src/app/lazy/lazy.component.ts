3f4809b99b13b3047293bdea61d7a2fddf9e1ac4
jimjones26 <jimjones26@gmail.com>
2017-03-17 15:31:39 -0400

    --

--- src/app/lazy/lazy.component.ts
+++ src/app/lazy/lazy.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
+import { CounterService } from './counter.service';
 
 @Component({
-  template: '<p>Lazy Component</p>'
+  template: `
+    <p>Lazy Component</p>
+    <button (click)="increaseCounter()">Increase Counter</button>
+    <p>Counter: {{ counterService.counter }}</p>`
 })
-export class LazyComponent { }
+export class LazyComponent {
+  constructor(public counterService: CounterService) {
+    console.log('lazy component constructor', this.counterService.counter);
+  }
+
+  increaseCounter() {
+    this.counterService.counter += 1;
+  }
+}
--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { LazyComponent } from './lazy.component';
 import { routing } from './lazy.routing';
+import { CounterService } from 'app/lazy/counter.service';
 
 @NgModule({
   imports: [routing],
-  declarations: [LazyComponent]
+  declarations: [LazyComponent],
+  providers: [CounterService]
 })
 export class LazyModule { }
