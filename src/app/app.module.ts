d56c40f1f262f1ecbd475388d2a1d431c1c52ca1
jimjones26 <jimjones26@gmail.com>
2017-03-16 22:51:58 -0400

    ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EagerComponent } from 'app/eager.component';
+import { routing } from 'app/app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EagerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    routing
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
